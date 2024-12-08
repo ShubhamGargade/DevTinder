a9a9f6f93456c5da29022ba6382a21561427faa9
Shubham Gargade <gargadeshubham@gmail.com>
2024-12-08 10:50:39 +0530

    added database key to contant.

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const {databaseKey} = require('../utils/constants.js')
 
 const connectDatabase = async() => {
-    await mongoose.connect('mongodb+srv://NodeJsLearning:OrpzcTABMzkYCKNQ@nodejslearning.zvoxw.mongodb.net/DevTinder;');
+    await mongoose.connect(`mongodb+srv://NodeJsLearning:${databaseKey}@nodejslearning.zvoxw.mongodb.net/DevTinder;`);
 }
 
 module.exports = connectDatabase;
