11eb0ddd91f52d28360369b3b646bedb5682cdde
Shubham Gargade <gargadeshubham@gmail.com>
2024-12-23 23:56:14 +0530

    Created mongodb-uri for db connection string.

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = async() => {
-    await mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_KEY}@nodejslearning.zvoxw.mongodb.net/DevTinder;`);
+    await mongoose.connect(process.env.MONGODB_URI);
 }
 
 module.exports = connectDatabase;
