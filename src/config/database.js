69d5b5f22588c3573e5ba071bd7a610db6aad88a
Shubham Gargade <gargadeshubham@gmail.com>
2024-12-23 23:49:02 +0530

    Added username and password to .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = async() => {
-    await mongoose.connect(`mongodb+srv://vercel-admin-user:TMqU4zqpVmfNGP61@nodejslearning.zvoxw.mongodb.net/DevTinder;`);
+    await mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_KEY}@nodejslearning.zvoxw.mongodb.net/DevTinder;`);
 }
 
 module.exports = connectDatabase;
