acad2a9c0cb9290eaf6f3e98b5ab91817751d56e
Shubham Gargade <gargadeshubham@gmail.com>
2024-12-23 16:35:10 +0530

    Added config for database url and created new vercel.json file for deployment.

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const {databaseKey} = require('../utils/constants.js')
+const {config} = require('../utils/constants.js')
 
 const connectDatabase = async() => {
-    await mongoose.connect(`mongodb+srv://NodeJsLearning:${databaseKey}@nodejslearning.zvoxw.mongodb.net/DevTinder;`);
+    await mongoose.connect(`mongodb+srv://NodeJsLearning:${config.databaseKey}@nodejslearning.zvoxw.mongodb.net/DevTinder;`);
 }
 
 module.exports = connectDatabase;
